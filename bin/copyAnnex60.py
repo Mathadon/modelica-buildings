033977acd83eeed5c0ab882bfdc2dc2af788612d
Michael Wetter <MWetter@lbl.gov>
2013-11-12 22:20:03 +0000

    Revised copy script.

--- bin/copyAnnex60.py
+++ bin/copyAnnex60.py
@@ -17,13 +17,19 @@ def replaceAnnex60String(filNam):
     f = open(filNam, 'r')
     lines = list()
     for lin in f.readlines():
-        lin = lin.replace("Modelica.Thermal.HeatTransfer.Sources.PrescribedHeatFlow",
-                          "Buildings.HeatTransfer.Sources.PrescribedHeatFlow")
-        lin = lin.replace("Modelica.Thermal.HeatTransfer.Sources.PrescribedTemperature",
-                          "Buildings.HeatTransfer.Sources.PrescribedTemperature")
-        lines.append(lin.replace("Annex60", "Buildings"))   
+        if filNam.endswith(".mo"):
+            lin = string.replace(lin, "Modelica.Thermal.HeatTransfer.Sources.PrescribedTemperature",
+                              "Buildings.HeatTransfer.Sources.PrescribedTemperature")
+            lin = string.replace(lin, "Modelica.Thermal.HeatTransfer.Sources.PrescribedHeatFlow",
+                              "Buildings.HeatTransfer.Sources.PrescribedHeatFlow")
+        lines.append(string.replace(lin, "Annex60", "Buildings"))   
     f.close
-    f = open(filNam, 'w')
+    if filNam.startswith(BLDREFRES):
+        # The reference results have the library in their file name.
+        f = open(filNam.replace("Annex60", "Buildings"), 'w')
+        os.remove(filNam)
+    else:
+        f = open(filNam, 'w')
     f.writelines(lines)
     f.close()
 
@@ -37,14 +43,16 @@ import shutil
 # Global settings
 ANNEX60HOME=os.path.abspath("/Users/mwetter/proj/ldrd/bie/modeling/github/iea-annex60/modelica-annex60/Annex60")
 BLDHOME=os.path.abspath("/Users/mwetter/proj/ldrd/bie/modeling/github/lbl-srg/modelica-buildings/Buildings")
-
+ANNEX60REFRES=os.path.join(ANNEX60HOME, "Resources", "ReferenceResults")
+BLDREFRES=os.path.join(BLDHOME, "Resources", "ReferenceResults")
 
 
 for root, dirs, files in os.walk(ANNEX60HOME):
     for fil in files:
         srcFil=os.path.join(root, fil)
         # Loop over all .mo files except for top-level .mo file
-        if srcFil.endswith(".mo") and not srcFil.endswith(os.path.join("Annex60", "package.mo")):
+        if (srcFil.endswith(".mo") and (not srcFil.endswith(os.path.join("Annex60", "package.mo")))) \
+            or (srcFil.endswith(".mos") or srcFil.startswith(ANNEX60REFRES)):
             desFil=srcFil.replace(ANNEX60HOME, BLDHOME)
             desPat=os.path.dirname(desFil)
             if not os.path.exists(desPat):
