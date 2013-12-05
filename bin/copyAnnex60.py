8670105cc382977fda0eb129331571818ddd0ad2
Michael Wetter <MWetter@lbl.gov>
2013-12-04 20:07:52 -0800

    Updated copy script.

--- bin/copyAnnex60.py
+++ bin/copyAnnex60.py
@@ -13,16 +13,42 @@ def replaceAnnex60String(filNam):
     :param filNam: Name of the file
     """
     import os
-    
+    rep = {"Annex60": 
+           "Buildings",
+           "Modelica.Thermal.HeatTransfer.Sources.PrescribedTemperature": 
+           "Buildings.HeatTransfer.Sources.PrescribedTemperature",
+           "Modelica.Thermal.HeatTransfer.Sources.PrescribedHeatFlow":
+           "Buildings.HeatTransfer.Sources.PrescribedHeatFlow"}
+
+#           "Buildings.Media.ConstantPropertyLiquidWater":
+#           "Buildings.Media.Water",
+#           "Buildings.Media.GasesPTDecoupled.MoistAir":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.GasesPTDecoupled.MoistAirUnsaturated":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.GasesPTDecoupled.SimpleAir":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.GasesConstantDensity.MoistAir":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.GasesConstantDensity.MoistAirUnsaturated":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.GasesConstantDensity.SimpleAir":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.IdealGases.SimpleAir":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.PerfectGases.MoistAir":
+#           "Buildings.Media.Air",
+#           "Buildings.Media.PerfectGases.MoistAirUnsaturated":
+#           "Buildings.Media.Air"
+
+
     f = open(filNam, 'r')
     lines = list()
     for lin in f.readlines():
-        if filNam.endswith(".mo"):
-            lin = string.replace(lin, "Modelica.Thermal.HeatTransfer.Sources.PrescribedTemperature",
-                              "Buildings.HeatTransfer.Sources.PrescribedTemperature")
-            lin = string.replace(lin, "Modelica.Thermal.HeatTransfer.Sources.PrescribedHeatFlow",
-                              "Buildings.HeatTransfer.Sources.PrescribedHeatFlow")
-        lines.append(string.replace(lin, "Annex60", "Buildings"))   
+        if filNam.endswith(".mo") or filNam.endswith(".mos"):
+            for ori, new in rep.iteritems():
+                lin = string.replace(lin, ori, new)
+        lines.append(lin)   
     f.close
     if filNam.startswith(BLDREFRES):
         # The reference results have the library in their file name.
@@ -33,7 +59,6 @@ def replaceAnnex60String(filNam):
     f.writelines(lines)
     f.close()
 
-
 import os, string, fnmatch, os.path, sys
 from os import makedirs
 from os.path import isfile, join
@@ -41,8 +66,9 @@ import shutil
 
 # --------------------------
 # Global settings
-ANNEX60HOME=os.path.abspath("/Users/mwetter/proj/ldrd/bie/modeling/github/iea-annex60/modelica-annex60/Annex60")
-BLDHOME=os.path.abspath("/Users/mwetter/proj/ldrd/bie/modeling/github/lbl-srg/modelica-buildings/Buildings")
+root = "/Users/mwetter/proj/ldrd/bie/modeling/github"
+ANNEX60HOME=os.path.abspath(root + "/iea-annex60/modelica-annex60/Annex60")
+BLDHOME=os.path.abspath(root + "/lbl-srg/modelica-buildings/Buildings")
 ANNEX60REFRES=os.path.join(ANNEX60HOME, "Resources", "ReferenceResults")
 BLDREFRES=os.path.join(BLDHOME, "Resources", "ReferenceResults")
 
@@ -50,27 +76,21 @@ BLDREFRES=os.path.join(BLDHOME, "Resources", "ReferenceResults")
 for root, dirs, files in os.walk(ANNEX60HOME):
     for fil in files:
         srcFil=os.path.join(root, fil)
-        # Loop over all .mo files except for top-level .mo file
+        # Loop over all 
+        # - .mo files except for top-level .mo file
+        # - package.order
+        # - .mos files
+        # - ReferenceResults
         if (srcFil.endswith(".mo") and (not srcFil.endswith(os.path.join("Annex60", "package.mo")))) \
-            or (srcFil.endswith(".mos") or srcFil.startswith(ANNEX60REFRES)):
+            or srcFil.endswith("package.order") \
+            or srcFil.endswith(".mos") \
+            or srcFil.startswith(ANNEX60REFRES) :
             desFil=srcFil.replace(ANNEX60HOME, BLDHOME)
             desPat=os.path.dirname(desFil)
             if not os.path.exists(desPat):
                 os.makedirs(desPat)
-            shutil.copy2(srcFil, desFil)
-            replaceAnnex60String(desFil)
+            if not srcFil.endswith("package.order") or \
+               srcFil.endswith("package.order") and not os.path.exists(desFil):
+                shutil.copy2(srcFil, desFil)
+                replaceAnnex60String(desFil)
         
-#    os.path.exists(path)
-#    filNam = helpDir + os.path.sep + fil
-#    filObj=open(filNam, 'r')
-#    lines = filObj.readlines()
-#    filObj.close()
-#    for old, new in replacements.iteritems():
-#        for i in range(len(lines)):
-#            lines[i] = lines[i].replace(old, new)
-#    filObj=open(filNam, 'w')
-#    for lin in lines:
-        # Check if line contains a wrong string
-#        validateLine(lin)
-#        filObj.write(lin)
-#    filObj.close()
